54421ff5311c92239af925d2553ef46d40061781
cdyreson <curtis.dyreson@usu.edu>
2024-09-18 10:49:52 -0600

    expOne script added

--- experimentalReproducibility/queryOneTrees.sh
+++ experimentalReproducibility/queryOneTrees.sh
@@ -1,5 +1,20 @@
-java -jar dist/TemporalKeywordSearch.jar --verbose --json trees20.json --disk --search "foo bar"
-java -jar dist/TemporalKeywordSearch.jar --verbose --json trees40.json --disk --search "foo bar"
-java -jar dist/TemporalKeywordSearch.jar --verbose --json trees60.json --disk --search "foo bar"
-java -jar dist/TemporalKeywordSearch.jar --verbose --json trees80.json --disk --search "foo bar"
-java -jar dist/TemporalKeywordSearch.jar --verbose --json trees100.json --disk --search "foo bar"
+for i in {1..5}
+do
+    java -jar dist/TemporalKeywordSearch.jar --verbose --json trees20.json --disk --search "foo bar"
+done
+for i in {1..5}
+do
+    java -jar dist/TemporalKeywordSearch.jar --verbose --json trees40.json --disk --search "foo bar"
+done
+for i in {1..5}
+do
+    java -jar dist/TemporalKeywordSearch.jar --verbose --json trees60.json --disk --search "foo bar"
+done
+for i in {1..5}
+do
+    java -jar dist/TemporalKeywordSearch.jar --verbose --json trees80.json --disk --search "foo bar"
+done
+for i in {1..5}
+do
+    java -jar dist/TemporalKeywordSearch.jar --verbose --json trees100.json --disk --search "foo bar"
+done
